7cc38e8fa92c4165de36cbe7d6525ad4728125f1
Shota Iizuka <45307692+iizus@users.noreply.github.com>
2024-12-05 21:45:07 +0900

    Update keyball.c

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 #ifdef SPLIT_KEYBOARD
-#    include "transactions.h"
+    #include "transactions.h"
 #endif
 
 #include "keyball.h"
